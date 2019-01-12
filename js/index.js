50b8a514f1821943016fb8bc69aacadfdc774c8a
ishtian revee <ishtian11@gmail.com>
2019-01-12 15:29:07 +0600

    handling user input, composing basic vue components

--- js/index.js
+++ js/index.js
@@ -4,12 +4,52 @@ var app = new Vue({
     message1: "Yaaoo!",
     message2: "Waddup Wordl!!!",
     message3: 'You loaded this page on ' + new Date().toLocaleString(),
+    message4: "I am awesome!",
+    message5: "Yaaoo Vue",
     seen: true,
-    todos: [
-      { text: 'HTML' },
-      { text: 'CSS' },
-      { text: 'JS' },
-      { text: 'Vue' }
+    todos: [{
+        text: 'HTML'
+      },
+      {
+        text: 'CSS'
+      },
+      {
+        text: 'JS'
+      },
+      {
+        text: 'Vue'
+      }
+    ],
+    grocerylist: [{
+        id: 0,
+        text: 'Fish'
+      },
+      {
+        id: 1,
+        text: 'Veges'
+      },
+      {
+        id: 2,
+        text: 'Drinks'
+      },
+      {
+        id: 3,
+        text: 'Cookies'
+      },
+      {
+        id: 4,
+        text: 'Meat'
+      }
     ]
+  },
+  methods: {
+    reverseString: function() {
+      this.message4 = this.message4.split('').reverse().join('')
+    }
   }
+})
+
+Vue.component('todo-item', {
+  props: ['todo'],
+  template: '<li>This is a text</li>'
 })
