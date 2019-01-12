bd5e7ceb5b764144e405a43ec5f021f8e5623b4f
ishtian revee <ishtian11@gmail.com>
2019-01-12 14:38:26 +0600

    v-bind, conditional statements and basic for loop added

--- js/index.js
+++ js/index.js
@@ -2,6 +2,14 @@ var app = new Vue({
   el: "#app",
   data: {
     message1: "Yaaoo!",
-    message2: "Waddup Wordl!!!"
+    message2: "Waddup Wordl!!!",
+    message3: 'You loaded this page on ' + new Date().toLocaleString(),
+    seen: true,
+    todos: [
+      { text: 'HTML' },
+      { text: 'CSS' },
+      { text: 'JS' },
+      { text: 'Vue' }
+    ]
   }
 })
