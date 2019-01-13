b37c9fdcc716f626d312992a592eae10b6a5b57d
ishtian revee <ishtian11@gmail.com>
2019-01-13 15:48:54 +0600

    little animation done using third party lib animate.css

--- projects/vue-skills/src/components/Skills.vue
+++ projects/vue-skills/src/components/Skills.vue
@@ -3,8 +3,11 @@
     <div class="holder">
       <form @submit.prevent="addSkill">
         <input type="text" placeholder="Enter a skill you have" v-model="skill" v-validate="'min:5'" name="skill">
-        <!-- 'errors' is an object from vee-validate library -->
-        <p class="alert" v-if="errors.has('skill')">{{ errors.first('skill') }}</p>
+
+        <transition name="alert-in" enter-active-class="animated flipInX" leave-active-class="animated flipOutX">
+          <!-- 'errors' is an object from vee-validate library -->
+          <p class="alert" v-if="errors.has('skill')">{{ errors.first('skill') }}</p>
+        </transition>
       </form>
 
       <ul>
@@ -47,6 +50,8 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+@import "https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.7.0/animate.min.css";
+
 .holder {
     background: #fff;
   }
@@ -91,5 +96,25 @@ export default {
     display: inline-block;
     padding: 5px;
     margin-top: -20px;
+  }
+
+  .alert-in-enter-active {
+    animation: bounce-in .5s;
+  }
+
+  .alert-in-leave-active {
+    animation: bounce-in .5s reverse;
+  }
+
+  @keyframes bounce-in {
+    0% {
+      transform: scale(0);
+    }
+    50% {
+      transform: scale(1.5);
+    }
+    100% {
+      transform: scale(1);
+    }
   }
 </style>
